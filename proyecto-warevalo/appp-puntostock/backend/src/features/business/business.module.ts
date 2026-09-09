16886c1e7d02d613399d8b24c0bd5f1cab8ddf89
Wesly Arevalo <weslyarevalo00@gmail.com>
2026-09-09 10:19:10 -0500

    crear carpeta del proyecto

--- proyecto-warevalo/appp-puntostock/backend/src/features/business/business.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ClientsModule } from './clients/clients.module.js';
-
-@Module({
-  imports: [ClientsModule],
-  exports: [ClientsModule],
-})
-export class BusinessModule {}
