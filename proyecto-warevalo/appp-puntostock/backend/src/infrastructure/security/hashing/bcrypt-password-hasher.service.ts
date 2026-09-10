1f572c9935daa62a0bcc882871f85e77c16e8117
Wesly Arevalo <weslyarevalo00@gmail.com>
2026-09-10 18:36:45 -0500

    infrastructure/security/hashing/bcrypt-password-hasher.service.ts

---
+++ proyecto-warevalo/appp-puntostock/backend/src/infrastructure/security/hashing/bcrypt-password-hasher.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+
+import * as bcrypt from 'bcrypt';
+
+import { IPasswordHasher } from './password-hasher.interface';
+
+@Injectable()
+export class BcryptPasswordHasherService
+  implements IPasswordHasher
+{
+  private readonly rounds = 10;
+
+  async hash(
+    plain: string,
+  ): Promise<string> {
+    return bcrypt.hash(
+      plain,
+      this.rounds,
+    );
+  }
+
+  async compare(
+    plain: string,
+    hashed: string,
+  ): Promise<boolean> {
+    return bcrypt.compare(
+      plain,
+      hashed,
+    );
+  }
+}
